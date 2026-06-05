de7ef8bb93dde35222438554202e0fe588026f58
TalianaAguirre <tsaguirre@uniboyaca.edu.co>
2026-06-05 17:19:45 -0500

    TalianaAguirre: [Modelos categoría y usuario de pedidos]

---
+++ ms-productos/app/Models/Producto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+        'disponible',
+        'categoria_id',
+    ];
+
+    protected $casts = [
+        'disponible' => 'boolean',
+    ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'categoria_id');
+    }
+}
